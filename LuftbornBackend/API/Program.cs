64a9fa098862b069ce5fb13a24f9acb517d3f0f3
Ahmed Ayoub <ahmed2000mostafa@gmail.com>
2025-02-28 04:05:45 +0200

    Initialized API Project

---
+++ LuftbornBackend/API/Program.cs
@@ -0,0 +1,32 @@
+using Application;
+using Infrastructure;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+builder.Services.AddOpenApi();
+
+builder.Services
+    .AddApplication()
+    .AddInfrastructure();
+
+builder.Services.AddDbContext<BlogDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"))
+);
+
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.MapOpenApi();
+}
+
+app.UseHttpsRedirection();
+
+app.Run();
