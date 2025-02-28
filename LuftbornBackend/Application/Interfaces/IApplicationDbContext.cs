c9e1a742e24110ac13310290c345670ebe8abce2
Ahmed Ayoub <ahmed2000mostafa@gmail.com>
2025-02-28 04:06:06 +0200

    Initialized Application Project

---
+++ LuftbornBackend/Application/Interfaces/IApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Application.Interfaces;
+
+public interface IApplicationDbContext
+{
+    DbSet<User> Users { get; set; }
+    DbSet<Post> Posts { get; set; }
+    DbSet<Comment> Comments { get; set; }
+
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken);
+}
